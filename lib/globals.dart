d305fd94d8d05c6445bba914871ada8445ff952b
Tim Ostermann <tim.ostermann@brickmakers.de>
2021-09-27 21:57:34 +0200

    [PBTO-25] add law of cosines

--- lib/globals.dart
+++ lib/globals.dart
@@ -22,10 +22,11 @@ double roundToNthDecimal(double number, int n) {
   return ((number * mod).round().toDouble() / mod);
 }
 
-double getBetaAngle(num a, num b, int? decimals) {
+double getBetaAngle(num aLength, num bLength, int? decimals) {
   return decimals != null
-      ? roundToNthDecimal(arcToSquareMeasure(math.atan(a / b)), decimals)
-      : arcToSquareMeasure(math.atan(a / b));
+      ? roundToNthDecimal(
+          arcToSquareMeasure(math.atan(aLength / bLength)), decimals)
+      : arcToSquareMeasure(math.atan(aLength / bLength));
 }
 
 double arcToSquareMeasure(double arc) {
@@ -36,8 +37,16 @@ double squareToArcMeasure(double square) {
   return square * (math.pi / 180);
 }
 
-double sineLaw(double alpha, double a, double beta) {
-  return (a / math.sin(alpha)) * math.sin(beta);
+// de: Sinussatz
+double lawOfSines(double alpha, double aLength, double beta) {
+  return (aLength / math.sin(alpha)) * math.sin(beta);
+}
+
+// de: Kosinussatz
+double lawOfCosines(double alpha, double beta, double gamma) {
+  return math.sqrt(math.pow(alpha, 2) +
+      math.pow(beta, 2) -
+      2 * alpha * beta * math.cos(squareToArcMeasure(gamma)));
 }
 
 double getAngleByAngleSum(double alpha, double beta) {
