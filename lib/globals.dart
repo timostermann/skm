a16961ceac0da9c78bea757b29686295303f6c36
Tim Ostermann <tim.ostermann@brickmakers.de>
2021-09-27 21:44:12 +0200

    [PBTO-25] add geometric helper functions

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 double getHeight(BuildContext context) {
   return MediaQuery.of(context).size.height;
@@ -14,4 +15,31 @@ bool isTablet(BuildContext context, [bool? useHeight]) {
   } else {
     return getWidth(context) > 700;
   }
+}
+
+double roundToNthDecimal(double number, int n) {
+  num mod = math.pow(10.0, n);
+  return ((number * mod).round().toDouble() / mod);
+}
+
+double getBetaAngle(num a, num b, int? decimals) {
+  return decimals != null
+      ? roundToNthDecimal(arcToSquareMeasure(math.atan(a / b)), decimals)
+      : arcToSquareMeasure(math.atan(a / b));
+}
+
+double arcToSquareMeasure(double arc) {
+  return arc * (180.0 / math.pi);
+}
+
+double squareToArcMeasure(double square) {
+  return square * (math.pi / 180);
+}
+
+double sineLaw(double alpha, double a, double beta) {
+  return (a / math.sin(alpha)) * math.sin(beta);
+}
+
+double getAngleByAngleSum(double alpha, double beta) {
+  return 180.0 - alpha - beta;
 }
